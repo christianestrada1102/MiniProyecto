8001602ade7b21f1bad44857772f0f74860eac02
JesusLeos16 <jelm060716@gmail.com>
2025-10-16 09:06:34 -0600

    Añadi el registro
    Apenas el esqueleto, solo que me falta ponerle que inicialice el proyecto desde el form "Registro"

--- MiniProyecto/MiniProyecto/Registro.cs
+++ MiniProyecto/MiniProyecto/Registro.cs
@@ -10,14 +10,14 @@ using System.Windows.Forms;
 
 namespace MiniProyecto
 {
-    public partial class Form1 : Form
+    public partial class Registro : Form
     {
-        public Form1()
+        public Registro()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Registro_Load(object sender, EventArgs e)
         {
 
         }
